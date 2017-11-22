6486d69eb8e7a79593c5a2d799f4a847cfa11cdf
Felix Cheruiyot <felix.cheruiyot@kenyaapps.net>
2017-11-22 11:40:54 +0300

    Setup

--- botforms/settings/base.py
+++ botforms/settings/base.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'botforms.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': '',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': ''
     }
 }
 
