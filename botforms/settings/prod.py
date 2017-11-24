5aa834514fed1c4c63239fb3ac5c9ecd2ce0e460
Felix Cheruiyot <felix.cheruiyot@kenyaapps.net>
2017-11-24 10:46:35 +0300

    Fix #2 #3 - .env file and email setting

--- botforms/settings/prod.py
+++ botforms/settings/prod.py
@@ -11,6 +11,21 @@ DATABASES['default'].update(db_from_env)
 # Production storage
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+# Email setting
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = os.environ.get('SMTP_SERVER')
+
+EMAIL_HOST_USER = os.environ.get('SMTP_USER')
+
+EMAIL_HOST_PASSWORD = os.environ.get('SMTP_PASSWORD')
+
+EMAIL_PORT = os.environ.get('SMTP_PORT', 587)
+
+EMAIL_USE_TLS = os.environ.get('SMTP_USE_TTL', True)
+
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_SITE_EMAIL')
+
 try:
     from .local import *
 except ImportError:
